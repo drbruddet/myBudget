a0b45453d3a5a655b69612a30b0d807c3a7ce91d
Guillaume Kolly <guillaume.kolly@gmail.com>
2016-12-27 16:24:40 +0100

    add first operation class

---
+++ src/client/app/components/OperationList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class OperationList extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Operation list</h2>
+      </div>
+    );
+  }
+}
+
+export default OperationList;
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+import React from 'react';
+import {render} from 'react-dom';
+
+import OperationList from './components/operationList.jsx';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>My Budget Application</h1>
+        <OperationList />
+      </div>
+    );
+  }
+}
+
+render(<App/>, document.getElementById('app'));
